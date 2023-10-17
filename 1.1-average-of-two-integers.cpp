20c70d22f3bc5ef24f7da25b85f2d1c2e29ca3ae
Saurlax <saurlax@qq.com>
2023-10-17 20:42:28 +0800

    add week 1

---
+++ 1.1-average-of-two-integers.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author Saurlax
+ * Write a program to output the average of 2 integers.
+ */
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%.2lf", (a + b) / 2.0);
+	return 0;
+}
